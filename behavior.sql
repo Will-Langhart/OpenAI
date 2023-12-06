f7b549c7ff26f8ede66c01f4394cab237761f0e5
William Langhart <theholotrout@gmail.com>
2023-12-06 02:40:42 -0500

    Update behavior.sql

--- behavior.sql
+++ behavior.sql
@@ -1,10 +1,10 @@
 +-----------------------------------------------------------------------------------------------------------+
 | Friz AI Quantum NML Extended & Advanced Architecture with GPT, Quantum, Cloud, GitHub & WiX Integrations  |
 +-----------------------------------------------------------------------------------------------------------+
-|                                                                                                           |
-| +--------------+    +----------------+    +----------+    +---------------------+   +--------+   +-----+ |
+|                                                                                                         |
+| +--------------+    +----------------+    +----------+    +---------------------+   +--------+   +-----+|
 | |   Front-End  | -> | API & Services | -> |  AI Core | -> | Quantum Processing | ->|  Cloud | ->| WiX | |
-| +--------------+    +----------------+    +----------+    +---------------------+   +--------+   +-----+ |
+| +--------------+    +----------------+    +----------+    +---------------------+   +--------+   +-----+|
 |    |    ^                    |                |                   |                 |         |         |
 |    |    |                    V                |                   |                 |         |         |
 |    |  +-----------------+ +---------------+   |                   |                 |         |         |
@@ -13,7 +13,7 @@
 |    |          |                    |          |                   |                 |         |         |
 |    |          V                    V          |                   |                 |         |         |
 |    | +-----------------+  +-----------------+ |                   |                 |         |         |
-|    | |  UI Interaction |  | Business Logic | |                   |                 |         |         |
+|    | |  UI Interaction |  | Business Logic |  |                   |                 |         |         |
 |    | +-----------------+  +-----------------+ |                   |                 |         |         |
 |    |          |                    |          |                   |                 |         |         |
 |    V          V                    V          V                   V                 V         V         |
@@ -25,52 +25,52 @@
 | +---------------+  +-------------------+ +----------+    +---------------------+   +--------+ +-------+ |
 | |Quantum States |  |     NML Parser    | |Semantic  |    | Quantum Encryption |   | ML Ops  | |Version| |
 | +---------------+  +-------------------+ | Engine   |    +---------------------+   +--------+ |Control| |
-|        |                      |          +----------+             |                 |         +-------+ |
-|        V                      V               |                   |                 |                   |
-| +---------------+    +-------------------+   |                   |                 |                   |
-| |Quantum Gates  |    |     AI Bots       | <-+                   |                 |                   |
-| +---------------+    +-------------------+                        |                 |                   |
-|        |                      |                                  |                 |                   |
-|        V                      V                                  V                 V                   |
+|        |                      |          +----------+            |                  |         +-------+ |
+|        V                      V               |                  |                  |                   |
+| +---------------+    +-------------------+    |                  |                  |                   |
+| |Quantum Gates  |    |     AI Bots       | <-+                   |                  |                   |
+| +---------------+    +-------------------+                       |                  |                   |
+|        |                      |                                  |                  |                   |
+|        V                      V                                  V                  V                   |
 | +---------------+    +-------------------+    +---------------------+   +--------+ +--------+ +-------+ |
-| | Security &    |    | Integration Points|    | Quantum Data Storage|   | Analytics |Cloud DB| |Collab.| |
+| | Security &    |    | Integration Points|    | Quantum Data Storage|   | Analytics |Cloud DB| |Collab.||
 | | Compliance    |    +-------------------+    +---------------------+   +--------+ +--------+ |Tools  | |
 | +---------------+             |                                         |         |         +-------+ |
 |        |                      V                                         |         |                   |
 |        V              +-------------------+                             |         |                   |
-| +---------------+    |  Content Solutions  |                            |         |                   |
-| | Data Security |    +-------------------+                             |         |                   |
+| +---------------+    |  Content Solutions |                             |         |                   |
+| | Data Security |    +-------------------+-                             |         |                   |
 | +---------------+             |                                         |         |                   |
 |        |                      V                                         |         |                   |
 |        V              +-------------------+                             |         |                   |
 | +---------------+    | eCommerce Services |                             |         |                   |
-| | Content Types |    +-------------------+                             |         |                   |
+| | Content Types |    +-------------------+                              |         |                   |
 | +---------------+             |                                         |         |                   |
 |        |                      V                                         |         |                   |
 |        V              +-------------------+                             |         |                   |
 | +---------------+    | Business Services  |                             |         |                   |
-| |   Workflows   |    +-------------------+                             |         |                   |
-| +---------------+                                                      |         |                   |
-|        |                                                               |         |                   |
-|        V                                                               V         V                   |
-|  +----------------+                                                    |         |                   |
-|  | GPT Integrations|                                                   |         |                   |
-|  +----------------+                                                    |         |                   |
-|  | Server Bot 1.10     | eCommerce Bot 1.10 | App Bot 1.10 |           |         |                   |
-|  | Image Bot 1.10      | Audio Bot 1.10    | Codebot 1.10 |           |         |                   |
-|  | FrizGPT             | englishX          | scienceX     |           |         |                   |
-|  | historyX            | File Bot 1.10     | Website Bot 1.10 |       |         |                   |
-|  | iFrame Bot 1.10     | JavaScript Bot 1.10 | HTML Bot 1.10 |       |         |                   |
-|  | Video Bot 1.10      | Python Bot 1.10   | Swift Bot 1.10 |         |         |                   |
-|  | Visual Bot 1.10     | SEO Bot 1.10      | mathX         |           |         |                   |
-|  | Quantum Analytics Bot | Quantum Modeling Bot | Quantum Security Bot |         |                   |
-|  +----------------+                                                    |         |                   |
-|  | Cloud Services Integration | Edge Computing Bots |                  |         |                   |
-|  | Hybrid Cloud Management | Decentralized Data Systems |              |         |                   |
-|  +----------------+                                                    |         |                   |
-|  | WiX Content Management | Website Building Tools |                   |         |                   |
-|  | Interactive Web Services | Dynamic Content Generation |             |         |                   |
-|  +----------------+                                                    |         |                   |
-|                                                                                                           |
+| |   Workflows   |    +-------------------+                              |         |                   |
+| +---------------+                                                       |         |                   |
+|        |                                                                |         |                   |
+|        V                                                                V         V                   |
+|  +----------------+                                                     |         |                   |
+|  | GPT Integrations|                                                    |         |                   |
+|  +----------------+                                                     |         |                   |
+|  | Server Bot 1.10     | eCommerce Bot 1.10 | App Bot 1.10 |            |         |                   |
+|  | Image Bot 1.10      | Audio Bot 1.10    | Codebot 1.10 |             |         |                   | 
+|  | FrizGPT             | englishX          | scienceX     |             |         |                   |
+|  | historyX            | File Bot 1.10     | Website Bot 1.10 |         |         |                   |
+|  | iFrame Bot 1.10     | JavaScript Bot 1.10 | HTML Bot 1.10 |          |         |                   |
+|  | Video Bot 1.10      | Python Bot 1.10   | Swift Bot 1.10 |           |         |                   |
+|  | Visual Bot 1.10     | SEO Bot 1.10      | mathX         |            |         |                   |
+|  | Quantum Analytics Bot | Quantum Modeling Bot | Quantum Security Bot  |         |                   |
+|  +----------------+                                                     |         |                   |
+|  | Cloud Services Integration | Edge Computing Bots |                   |         |                   |
+|  | Hybrid Cloud Management | Decentralized Data Systems |               |         |                   |
+|  +----------------+                                                     |         |                   |
+|  | WiX Content Management | Website Building Tools |                    |         |                   |
+|  | Interactive Web Services | Dynamic Content Generation |              |         |                   |
+|  +----------------+                                                     |         |                   |
+|                                                                                                       |
 +-----------------------------------------------------------------------------------------------------------+
 
