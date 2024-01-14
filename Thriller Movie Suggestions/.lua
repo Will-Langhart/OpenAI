b81972a135991d78a2e179424ae174e585d94e83
William C. Langhart <theholotrout@gmail.com>
2024-01-13 19:37:35 -0500

    Create .lua

---
+++ Thriller Movie Suggestions/.lua
@@ -0,0 +1,94 @@
+               Movie Thriller/Horror List:
+                      🎬By FrizAI
+-—————————————————————————————————————————————————|
+ Rank: Movie:  Disturbing rank             (0-10) |
+-—————————————————————————————————————————————————|
+1. Shutter Island                            (8)  |               
+2. The Invitation                            (9)  |                       
+3. Hereditary                               (10)  |                      
+4. Apocalypse Now                            (8)  |                      
+5. The Thing                                 (8)  |                      
+6. Prisoners                                 (9)  |                      
+7. Apocalypse Now                            (8)  |                       
+8. The gift                                  (7)  |                       
+9. The Game                                  (7)  |                     
+10. Night crawler                            (8)  |                   
+11. The machinist                            (8)  |                    
+12. Hell House LLC                           (8)  |               
+13. Coherence                                (6)  |                  
+14. Ex machina                               (5)  |                  
+15. Memento                                  (5)  |                                 
+——————————————————————————————————————————————————|
+
+       Produced in 2021-Present:
+		            😱= Must Watch
+The Descent 😱
+Tekkonkinkreek😱
+Perfect Blue 
+Deliverance
+Evil Dead 😱
+Don’t Breathe 😱
+The Perfection  (SOLID BUT TWISTED)
+The Mist 
+Bone Tomahawk 
+Poseidon 
+The Revenant 
+Apostle 😱 Ex
+Kill bill 
+Pulp fiction 
+Southpaw 
+Fight club 
+The lighthouse 😱
+1917
+Knives out
+Uncut gems 
+Primer 
+The killing of a sacred deer 
+Once upon a time in Hollywood 😱
+Fractured 
+Platform 
+The Big Lebowski 😱
+Mystic river 
+The invisible guest 
+Train to Busan 
+Parasite 😱
+Tusk
+Good Time 
+Climax ￼😱
+The Blackoats Daughter 
+M.O.M (Mothers of Monsters)
+Sea Fever 
+Lifechanger 
+The house of the devil 
+Population 436
+Splice 
+Apocalypto 
+Better watch out 
+1BR
+The taking of Deborah Logan 
+Haunt
+Possessor 
+DeathGasm
+The dark and wicked 
+Saint Maud 
+Splinter
+Malignant
+VHS 94
+Last Night in SoHo 
+It Come’s at Night
+Last night in Soho 
+Paranormal Activity Next of Kin 
+Saint Maud 
+Splinter
+Malignant
+VHS 92
+Last Night in SoHo
+Terrifier 2
+Barbarian 
+
+                 Produced in 2023:
+The Menu
+Infinity Pooll
+Letters from iwo jima 
+Everything Everywhere All At Once 😱
+
